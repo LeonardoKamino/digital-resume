4d377d2edea9ca198539dec79f1b72c8a16f502e
Leonardo Kamino <leo.kamino@gmail.com>
2021-08-02 19:47:24 -0700

    Update Projects

--- src/Components/ProjectsList.tsx
+++ src/Components/ProjectsList.tsx
@@ -1,5 +1,6 @@
 import PersonalSite from "../Media/PersonalSite.png";
 import Hireflow from "../Media/Hireflow.png";
+import SortingVisualizer from "../Media/SortingVisualizer.png";
 import MoreComing from "../Media/Loading.png";
 
 const ProjectsList = [
@@ -18,8 +19,17 @@ const ProjectsList = [
 		description:
 			"This is my personal website. Its objective is to work as my digital resume / portifolio, to show my experiences and projects I already worked on.",
 		skills: ["Typescript", "React", "HTML", "CSS/Sass", "Bootstrap 5"],
-		deploymentLink: "https://www.google.com/",
-		githubLink: "https://github.com/",
+		deploymentLink: "",
+		githubLink: "https://github.com/LeonardoKamino/digital-resume",
+	},
+	{
+		title: "sortingvisualizer.com",
+		image: SortingVisualizer,
+		description:
+			"The objective of this project is to help me improve my front end skills, while learning more about sorting algorithms. My expectations is to keep it as a on going project, updating it as I learn more algorithms.",
+		skills: [ "Sorting Algorithms", "HTML", "CSS", "JavaScript"],
+		deploymentLink: "https://leo-sortingvisualizer.netlify.app",
+		githubLink: "https://github.com/LeonardoKamino/sorting_visualizer",
 	},
 
 	{
